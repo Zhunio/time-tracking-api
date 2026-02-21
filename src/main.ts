b0e47f1f677a7b52b254b5c79393b27cd93f7cab
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2026-02-20 21:09:29 -0500

    Enable CORS in app bootstrap

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: true,
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders: '*',
+    exposedHeaders: '*',
+    credentials: true,
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Time Tracking API')
     .setDescription('API for users, auth, and time trackers')
